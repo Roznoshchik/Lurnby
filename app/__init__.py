95f20ac960f78f995fd32e3debd05c1fb576efc9
rr <roznoshchik@gmail.com>
2020-05-22 10:35:05 +0300

    db logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 import os
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from app import routes, models
+
