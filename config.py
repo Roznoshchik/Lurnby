ab7cef54c568b979e0029ed808ab0f3140914240
rr <roznoshchik@gmail.com>
2020-05-22 10:34:21 +0300

    added db logic for Users table

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Slava-fakes-it-till-he-makes-it'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
