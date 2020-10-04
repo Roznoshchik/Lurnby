d954b8a1f450725054ef8ac933ee2b9589f5aae8
rr <roznoshchik@gmail.com>
2020-10-04 21:27:05 +0300

    update for storing type preferences

--- app/main/routes.py
+++ app/main/routes.py
@@ -256,12 +256,12 @@ def filter_articles():
 @login_required
 def reader_preferences():
     if request.method == "POST":
-        current_user.preferences = json.loads(request.form['Preferences'])
+        current_user.preferences = json.dumps(request.form['Preferences'])
         db.session.commit()
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}  
     
     if request.method == "GET":
-        preferences = current_user.preferences
+        preferences = json.loads(current_user.preferences)
         
         return jsonify({ 'Preferences': preferences })
 
