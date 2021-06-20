f3e612a8f6b4e482dee70b7bc9db10137f341c97
rr <roznoshchik@gmail.com>
2021-06-20 18:05:59 +0300

    changed selectedText.toString() to selectedObj.toString() for formatting highlight selection

--- app/static/highlight.js
+++ app/static/highlight.js
@@ -54,7 +54,7 @@ function textActions(event) {
     selectedText = selectedObj.getRangeAt(0);
 
 
-    console.log(selectedText)
+    //console.log(selectedText)
     
     var rect = selectedText.getBoundingClientRect();
     
@@ -109,10 +109,9 @@ $('#addhighlight').click(function(){
 
 $('#AddHighlightModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
-    
+    var rawhighlight = selectedObj.toString() // selected text
     selectedObj.removeAllRanges();
 
-    var rawhighlight = selectedObj.toString() // selected text
     var highlight_position = position; 
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
