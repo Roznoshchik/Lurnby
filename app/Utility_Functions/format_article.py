2c086bf30e7c7545541ce82a4c27e473be3befb2
rr <roznoshchik@gmail.com>
2021-08-22 14:24:43 +0300

    saving function I used for fixing preformatted book

---
+++ app/Utility_Functions/format_article.py
@@ -0,0 +1,31 @@
+# set empty content
+content = []
+# get content and split it at the pre tag
+with open("content.html", "r") as x:
+    text = x.read()
+    content = text.split('</pre>')
+#create empty string
+html = ''
+for block in content:
+    # each block is a pre element  
+    html += '<div>' # open div
+    block = block.replace('<pre>','') # remove old pre
+    paragraphs = block.split('\n') # split block on each new line for paragrapgs
+    new_para='' # empty para string
+    for p in paragraphs:
+        new_para += f'<p>{p}</p>' # add p tags
+    block = new_para.split('<p></p>') # split at empty paragraphs
+    for b in block:
+        html += f'<div>{b}</div>' # wrap sections in divs
+    html += '</div>'
+
+content = html.split('<div></div>') # split at empty divs
+
+with open('newfile.html', 'w') as f:
+    for block in content:
+        f.write(block) # write to file
+      
+    
+
+
+        
