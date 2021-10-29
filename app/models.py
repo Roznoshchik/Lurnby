711d2f843f26d110fa26509bfd19235cc6a28301
rr <roznoshchik@gmail.com>
2021-10-29 15:20:05 +0300

    added event class

--- app/models.py
+++ app/models.py
@@ -32,6 +32,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     articles = db.relationship('Article', backref='user', lazy='dynamic')
     highlights = db.relationship('Highlight', backref='user', lazy='dynamic')
+    events = db.relationship('Event', backref="user", lazy='dynamic')
     approved_senders = db.relationship('Approved_Sender', backref='user',
                                        lazy='dynamic')
     topics = db.relationship('Topic', backref='user', lazy='dynamic')
@@ -168,6 +169,13 @@ class User(UserMixin, db.Model):
         n = Notification(name=name, payload_json=json.dumps(data), user=self)
         db.session.add(n)
         return n
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String())
+    date = db.Column(db.DateTime())
+
 
 class Approved_Sender(db.Model):
     id = db.Column(db.Integer, primary_key=True)
