bc3b231d9b27b5600da70698e2a657d4d6b2c35e
rr <roznoshchik@gmail.com>
2021-11-05 16:07:43 +0200

    css and js changes to settings pages

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -1,5 +1,42 @@
-byId('test').innerText = 'hello from settings.js'
+var menu = byId('settings_menu')
+
+var move = (menu.scrollWidth - menu.clientWidth) / 2 
+
+var left = byClass('nav_left')[0]
+var right = byClass('nav_right')[0]
+
+left.addEventListener('click', () => {menu.scrollLeft -= move} )
+right.addEventListener('click', () => {menu.scrollLeft += move} )
+
+function button_viz(){
+    // if (menu.scrollLeft == 0) {
+    //     left.style.display = "none"
+    //     right.style.display = "block"
+    // }
+    // else if(menu.scrollLeft == menu.scrollWidth - menu.clientWidth){
+    //     left.style.display = "block"
+    //     right.style.display = "none"
+    // }
+    // else {
+    //     left.style.display = "block"
+    //     right.style.display = "block"
+    // }
+
+    if (menu.scrollLeft == 0) {
+        left.style.opacity = 0
+        right.style.opacity = 100
+    }
+    else if(menu.scrollLeft == menu.scrollWidth - menu.clientWidth){
+        left.style.opacity = 100
+        right.style.opacity = 0
+    }
+    else {
+        left.style.opacity = 100
+        right.style.opacity = 100
+    }
 
 
 
+}
 
+menu.addEventListener('scroll', () => {button_viz()})
