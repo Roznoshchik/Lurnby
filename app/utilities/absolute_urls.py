78d0a74c2ee5702d5f4975f0156d6572d4608faf
rr <roznoshchik@gmail.com>
2021-11-16 00:08:55 +0200

    renamed utilities

---
+++ app/utilities/absolute_urls.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+from app import db
+
+
+def set_absolute_urls(*articles):
+    for a in articles:
+        if a.source_url:
+            soup = BeautifulSoup(a.content, "html5lib")
+            images = soup.find_all("img")
+            for img in images:
+                try:
+                    if 'http' not in img['src']:
+                        img['src'] = f'{a.source_url}{img["src"]}'
+                except:
+                    print(f'error with article: {a.uuid}\n img: \n{img}')
+            links = soup.find_all('a')
+            for l in links:
+                try:
+                    if 'http' not in l['href']:
+                        l['href'] = f'{a.source_url}{l["href"]}'
+                except:
+                    print(f'error with article: {a.uuid}\n url: \n{l}')
+
+            a.content = str(soup.prettify())
+    db.session.commit()
+
+
+
+def fix_article_note_links(a):
+    if a.notes and a.notes != '':
+        soup = BeautifulSoup(a.notes, "html5lib")
+        links = soup.find_all('a')
+        for l in links:
+            try:
+                if '/article/' not in l['href']:
+                    l['href'] = f'/article/{l["href"]}'
+            except:
+                print(f'error with article: {a.uuid}\n url: \n{l}')
+        a.notes = str(soup.prettify())
+    db.session.commit()
---
+++ app/utilities/protect_images.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+from app import db
+from app.models import Article
+
+def protect_images():
+    articles = Article.query.all()
+    for a in articles:
+        soup = BeautifulSoup(a.content, "html.parser")
+        images = soup.find_all('img')
+        for img in images:
+            try:
+                if "/download/" in img['src']:
+                    u = img['src'].replace('/download/', f'/download/{a.user_id}/')
+                    img['src'] = u
+            except:
+                pass
+        a.content = str(soup.prettify())
+    db.session.commit()
