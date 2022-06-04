d88446ec612c16e5745ca49230432f790b218d1d
RR <roznoshchik@gmail.com>
2022-06-04 12:13:10 +0200

    style: fix for dark mode pre, added temp to gitignore

---
+++ app/utilities/match_tags.py
@@ -0,0 +1,16 @@
+from app import db
+from app.models import Article, Tag
+
+def match_tags():
+  articles = Article.query.all()
+  for a in articles:
+    tags = a.tags.all()
+    for t in tags:
+      if t.user_id != a.user_id:
+        a.RemoveFromTag(t)
+        tag = Tag.query.filter_by(name=t.name, user_id=a.user_id).first()
+        if not tag:
+          tag = Tag(name=t.name, user_id=a.user_id)
+          db.session.add(tag)
+        a.AddToTag(tag)
+  db.session.commit()
