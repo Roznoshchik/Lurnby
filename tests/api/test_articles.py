43a6a6f9e932de3483cf42bdcf53edf5f47c5e55
RR <roznoshchik@gmail.com>
2023-01-20 23:14:30 -0500

    formatted tests with black

--- tests/api/test_articles.py
+++ tests/api/test_articles.py
@@ -11,13 +11,12 @@ from ..mocks.mocks import mock_articles, mock_tags
 
 class TestConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
 
 
 class UserApiTests(unittest.TestCase):
-
     def setUp(self):
-        os.environ['testing'] = '1'
+        os.environ["testing"] = "1"
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
@@ -25,7 +24,7 @@ class UserApiTests(unittest.TestCase):
         db.create_all()
 
         # setup user
-        user = User(email='test@test.com')
+        user = User(email="test@test.com")
         db.session.add(user)
         db.session.commit()
 
@@ -40,10 +39,10 @@ class UserApiTests(unittest.TestCase):
         for article in mock_articles:
             a = Article()
             for key in article:
-                if key != 'tags':
+                if key != "tags":
                     setattr(a, key, article[key])
 
-            for tag in article['tags']:
+            for tag in article["tags"]:
                 t = Tag.query.filter_by(name=tag).first()
             db.session.add(a)
             a.AddToTag(t)
@@ -55,221 +54,294 @@ class UserApiTests(unittest.TestCase):
         db.drop_all()
         self.app_context.pop()
 
-    @patch('app.models.User.check_token')
+    @patch("app.models.User.check_token")
     def test_get_articles(self, mock_check_token):
         mock_check_token.return_value = User.query.first()
 
         # first check default which should return 4 unarchived articles
         params = {}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(4, len(data['articles']))
+        self.assertEqual(4, len(data["articles"]))
 
         # then check for archived articles
-        params = {"status": 'archived'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"status": "archived"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(1, len(data['articles']))
+        self.assertEqual(1, len(data["articles"]))
 
         # then check for unread articles
-        params = {"status": 'unread'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"status": "unread"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(1, len(data['articles']))
+        self.assertEqual(1, len(data["articles"]))
 
         # then check for read articles
-        params = {"status": 'read'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"status": "read"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(1, len(data['articles']))
+        self.assertEqual(1, len(data["articles"]))
 
         # then check for in_progress articles
-        params = {"status": 'in_progress'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"status": "in_progress"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(2, len(data['articles']))
+        self.assertEqual(2, len(data["articles"]))
 
         # then check search title
-        params = {"q": 'baz'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"q": "baz"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(1, len(data['articles']))
+        self.assertEqual(1, len(data["articles"]))
 
         # then check search content
-        params = {"q": 'love'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"q": "love"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(2, len(data['articles']))
+        self.assertEqual(2, len(data["articles"]))
 
         # then check filter by tags
-        params = {"tag_ids": '1,3'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"tag_ids": "1,3"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(3, len(data['articles']))
+        self.assertEqual(3, len(data["articles"]))
 
         # then check sorting by title ascending
-        params = {"title_sort": 'ASC'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"title_sort": "ASC"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual('baba', data['articles'][0]['title'])
-        self.assertEqual('bar', data['articles'][1]['title'])
-        self.assertEqual('baz', data['articles'][2]['title'])
-        self.assertEqual('foo', data['articles'][3]['title'])
+        self.assertEqual("baba", data["articles"][0]["title"])
+        self.assertEqual("bar", data["articles"][1]["title"])
+        self.assertEqual("baz", data["articles"][2]["title"])
+        self.assertEqual("foo", data["articles"][3]["title"])
 
         # then check sorting by title descending
-        params = {"title_sort": 'desc'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"title_sort": "desc"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual('foo', data['articles'][0]['title'])
-        self.assertEqual('baz', data['articles'][1]['title'])
-        self.assertEqual('bar', data['articles'][2]['title'])
-        self.assertEqual('baba', data['articles'][3]['title'])
+        self.assertEqual("foo", data["articles"][0]["title"])
+        self.assertEqual("baz", data["articles"][1]["title"])
+        self.assertEqual("bar", data["articles"][2]["title"])
+        self.assertEqual("baba", data["articles"][3]["title"])
 
         # then check sorting by date_read descending
-        params = {"opened_sort": 'DESC'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"opened_sort": "DESC"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual('baba', data['articles'][0]['title'])
-        self.assertEqual('baz', data['articles'][1]['title'])
-        self.assertEqual('bar', data['articles'][2]['title'])
-        self.assertEqual('foo', data['articles'][3]['title'])
+        self.assertEqual("baba", data["articles"][0]["title"])
+        self.assertEqual("baz", data["articles"][1]["title"])
+        self.assertEqual("bar", data["articles"][2]["title"])
+        self.assertEqual("foo", data["articles"][3]["title"])
 
         # then check sorting by date_read ascending
-        params = {"opened_sort": 'asc'}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        params = {"opened_sort": "asc"}
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual('foo', data['articles'][0]['title'])
-        self.assertEqual('bar', data['articles'][1]['title'])
-        self.assertEqual('baz', data['articles'][2]['title'])
-        self.assertEqual('baba', data['articles'][3]['title'])
+        self.assertEqual("foo", data["articles"][0]["title"])
+        self.assertEqual("bar", data["articles"][1]["title"])
+        self.assertEqual("baz", data["articles"][2]["title"])
+        self.assertEqual("baba", data["articles"][3]["title"])
 
         # then check that pagination returns 1 and has_next
         params = {"per_page": "1"}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(1, len(data['articles']))
-        self.assertIsNotNone(data['has_next'])
+        self.assertEqual(1, len(data["articles"]))
+        self.assertIsNotNone(data["has_next"])
 
         # then check that pagination returns 4 and has_next is empty
         params = {"per_page": "4"}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(4, len(data['articles']))
-        self.assertIsNone(data['has_next'])
+        self.assertEqual(4, len(data["articles"]))
+        self.assertIsNone(data["has_next"])
 
         # then check that pagination returns all and has_next is empty
         params = {"per_page": "all"}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(4, len(data['articles']))
-        self.assertIsNone(data['has_next'])
+        self.assertEqual(4, len(data["articles"]))
+        self.assertIsNone(data["has_next"])
 
         # then check that we can get the last page
         params = {"per_page": "1", "page": "4"}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(1, len(data['articles']))
-        self.assertIsNone(data['has_next'])
+        self.assertEqual(1, len(data["articles"]))
+        self.assertIsNone(data["has_next"])
 
         # then check that we can get the 2nd page and has_next is not empty
         params = {"per_page": "1", "page": "2"}
-        res = self.client.get('/api/articles', query_string=params, headers={'Authorization': 'Bearer abc123'})
+        res = self.client.get(
+            "/api/articles",
+            query_string=params,
+            headers={"Authorization": "Bearer abc123"},
+        )
         data = json.loads(res.data)
-        self.assertEqual(1, len(data['articles']))
-        self.assertIsNotNone(data['has_next'])
+        self.assertEqual(1, len(data["articles"]))
+        self.assertIsNotNone(data["has_next"])
 
-    
-    @patch('app.models.User.check_token')
+    @patch("app.models.User.check_token")
     def test_add_supplied_epub_article(self, mock_check_token):
         mock_check_token.return_value = User.query.first()
-        mock_file = open(f'{Path(os.path.dirname(__file__)).parent}/mocks/mock.epub', 'rb')
-        payload = {'tags': ["pikachu"]}
-        
+        mock_file = open(
+            f"{Path(os.path.dirname(__file__)).parent}/mocks/mock.epub", "rb"
+        )
+        payload = {"tags": ["pikachu"]}
+
         res = self.client.post(
-            '/api/articles', 
-            data={"file": mock_file, "data": json.dumps(payload)}, 
-            headers={'Authorization': 'Bearer abc123'})
-        
+            "/api/articles",
+            data={"file": mock_file, "data": json.dumps(payload)},
+            headers={"Authorization": "Bearer abc123"},
+        )
+
         data = res.json
         article = Article.query.filter_by(id=6).first()
         tag = article.tags.all()[0]
-        
-        self.assertEqual(res.status_code, 201)
-        self.assertEqual(6, data['article_id'])
-        self.assertTrue('task_id' in data)
-        self.assertTrue(data['processing'])
-        self.assertEqual(tag.name, 'pikachu')
 
-    
-    @patch('app.models.User.check_token')
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(6, data["article_id"])
+        self.assertTrue("task_id" in data)
+        self.assertTrue(data["processing"])
+        self.assertEqual(tag.name, "pikachu")
+
+    @patch("app.models.User.check_token")
     def test_bad_data_returns_400(self, mock_check_token):
         mock_check_token.return_value = User.query.first()
-        
+
         # empty payload
-        payload = {'tags': ["pikachu", "bulbasaur", "charmander"]}
+        payload = {"tags": ["pikachu", "bulbasaur", "charmander"]}
 
         res = self.client.post(
-            '/api/articles', 
-            data={"data": json.dumps(payload)}, 
-            headers={'Authorization': 'Bearer abc123'})
-        
+            "/api/articles",
+            data={"data": json.dumps(payload)},
+            headers={"Authorization": "Bearer abc123"},
+        )
+
         data = res.json
         self.assertEqual(res.status_code, 400)
-        self.assertEqual('No article to create. Check data and try again', data['message'])
-        
+        self.assertEqual(
+            "No article to create. Check data and try again", data["message"]
+        )
+
         # bad url
-        payload = {'url': 'foo.bar'}
+        payload = {"url": "foo.bar"}
 
         res = self.client.post(
-            '/api/articles', 
-            data={"data": json.dumps(payload)}, 
-            headers={'Authorization': 'Bearer abc123'})
-        
+            "/api/articles",
+            data={"data": json.dumps(payload)},
+            headers={"Authorization": "Bearer abc123"},
+        )
+
         data = res.json
         self.assertEqual(res.status_code, 400)
-        self.assertEqual("Can't validate url. Please check the data and try again", data['message'])
-        
+        self.assertEqual(
+            "Can't validate url. Please check the data and try again", data["message"]
+        )
+
         # bad url that passes validators check
-        payload = {'url': 'https://www.lurnby.com/static/images/rrfeedback-40.png'}
+        payload = {"url": "https://www.lurnby.com/static/images/rrfeedback-40.png"}
 
         res = self.client.post(
-            '/api/articles', 
-            data={"data": json.dumps(payload)}, 
-            headers={'Authorization': 'Bearer abc123'})
-        
+            "/api/articles",
+            data={"data": json.dumps(payload)},
+            headers={"Authorization": "Bearer abc123"},
+        )
+
         data = res.json
         self.assertEqual(res.status_code, 400)
-        self.assertEqual("Something went wrong. Please check the url", data['message'])
-        
+        self.assertEqual("Something went wrong. Please check the url", data["message"])
+
         # manual submission missing data
-        payload = {'manual_entry': {"pikachu": "was here"}}
+        payload = {"manual_entry": {"pikachu": "was here"}}
 
         res = self.client.post(
-            '/api/articles', 
-            data={"data": json.dumps(payload)}, 
-            headers={'Authorization': 'Bearer abc123'})
-        
+            "/api/articles",
+            data={"data": json.dumps(payload)},
+            headers={"Authorization": "Bearer abc123"},
+        )
+
         data = res.json
         self.assertEqual(res.status_code, 400)
-        self.assertEqual("Missing Title or Content", data['message'])
-        
+        self.assertEqual("Missing Title or Content", data["message"])
 
         # non pdf or no epub file
-        mock_file = open(f'{Path(os.path.dirname(__file__)).parent}/mocks/mocks.py', 'rb')
-        
-        payload = {'tags': ["pikachu", "bulbasaur", "charmander"]}
-        
+        mock_file = open(
+            f"{Path(os.path.dirname(__file__)).parent}/mocks/mocks.py", "rb"
+        )
+
+        payload = {"tags": ["pikachu", "bulbasaur", "charmander"]}
+
         res = self.client.post(
-            '/api/articles', 
-            data={"file": mock_file, "data": json.dumps(payload)}, 
-            headers={'Authorization': 'Bearer abc123'})
-        
+            "/api/articles",
+            data={"file": mock_file, "data": json.dumps(payload)},
+            headers={"Authorization": "Bearer abc123"},
+        )
+
         data = res.json
         self.assertEqual(res.status_code, 400)
-        self.assertEqual('File must be pdf or epub', data['message'])
-
-     
-        
-        
-    
-    
+        self.assertEqual("File must be pdf or epub", data["message"])
--- tests/mocks/mocks.py
+++ tests/mocks/mocks.py
@@ -2,55 +2,55 @@ import datetime
 
 mock_articles = [
     {
-        'user_id': 1,
-        'title': 'foo',
-        "content": 'alabama arkansas, I do love my ma and pa',
+        "user_id": 1,
+        "title": "foo",
+        "content": "alabama arkansas, I do love my ma and pa",
         "archived": False,
         "done": True,
         "unread": False,
         "date_read": datetime.datetime(2023, 1, 6, 0, 33, 5, 792506),
-        "tags": ['banana']
+        "tags": ["banana"],
     },
     {
-        'user_id': 1,
-        'title': 'bar',
-        "content": 'At first I was afraid, I was petrified, thinking I could never love again',
+        "user_id": 1,
+        "title": "bar",
+        "content": "At first I was afraid, I was petrified, thinking I could never love again",
         "archived": False,
         "done": False,
         "unread": True,
         "date_read": datetime.datetime(2023, 1, 10, 0, 33, 5, 792506),
-        "tags": ['banana']
+        "tags": ["banana"],
     },
     {
-        'user_id': 1,
-        'title': 'baz',
+        "user_id": 1,
+        "title": "baz",
         "content": "I just can't get enough",
         "archived": False,
         "done": False,
         "unread": False,
         "date_read": datetime.datetime(2023, 1, 11, 0, 33, 5, 792506),
-        "tags": ['peaches']
+        "tags": ["peaches"],
     },
     {
-        'user_id': 1,
-        'title': 'baba',
+        "user_id": 1,
+        "title": "baba",
         "content": "All around me are familiar places",
         "archived": False,
         "done": False,
         "unread": False,
         "date_read": datetime.datetime(2023, 1, 22, 0, 33, 5, 792506),
-        "tags": ['pears']
+        "tags": ["pears"],
     },
     {
-        'user_id': 1,
-        'title': 'kazoo',
+        "user_id": 1,
+        "title": "kazoo",
         "content": "You are my sunshine",
         "archived": True,
         "done": False,
         "unread": False,
         "date_read": datetime.datetime(2023, 1, 12, 0, 33, 5, 792506),
-        "tags": ['peaches']
+        "tags": ["peaches"],
     },
 ]
 
-mock_tags = ['banana', 'peaches', 'pears', 'strawberries']
+mock_tags = ["banana", "peaches", "pears", "strawberries"]
