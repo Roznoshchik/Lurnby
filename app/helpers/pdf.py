7d229b70bd805c4df746df66bf595904ac504cd3
RR <roznoshchik@gmail.com>
2023-02-25 17:38:11 -0500

    refactored pdf.py and added unit tests

--- app/helpers/pdf.py
+++ app/helpers/pdf.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import os
 
 import fitz
@@ -6,144 +7,116 @@ from werkzeug.utils import secure_filename
 
 from app import s3, bucket
 
+IMAGE = 1
 
-def importPDF(file, u=None):
+
+def importPDF(file, user=None):
 
     filepath = file.filename if hasattr(file, "filename") else file
-
-    if not u:
-        u = current_user
     doc = fitz.open(filepath)
+    title, title_for_dir = get_titles(doc)
 
-    content = ""
-    title = ""
     processed = {}
+    content = ""
 
-    # get all font sizes in doc
-    sizes = {}
+    url_dict = process_images_and_get_url_dict(doc, title_for_dir, user)
+    sizes = create_sizes_to_header_tags_dict(doc)
+
     for page in doc:
-        p = page.get_text("dict")["blocks"]
-        for i in p:
-            if i["type"] == 0:
-                lines = i["lines"]
+        page_contents = page.get_text("dict")["blocks"]
+        for item in page_contents:
+            if item["type"] == IMAGE:
+                content += url_dict.get(item["number"], "")
+            else:
+                lines = item.get("lines", {})
+
                 for line in lines:
                     spans = line["spans"]
                     for span in spans:
-                        sizes[span["size"]] = span["size"]
-        for i in sizes:
-            if i > 40:
-                sizes[i] = "h1"
-            if i < 40 and i > 30:
-                sizes[i] = "h2"
-            if i < 30 and i > 25:
-                sizes[i] = "h3"
-            if i < 25 and i > 20:
-                sizes[i] = "h4"
-            if i < 20 and i > 16:
-                sizes[i] = "h5"
-            else:
-                sizes[i] = "p"
-
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(basedir, "temp")
-
-    if not os.path.isdir(path):
-        os.mkdir(path)
-
-    title = doc.metadata["title"]
-    if title == "":
-        title = doc.load_page(0).get_text()[:155] + "..."
-
-    title_for_dir = secure_filename(title)
-    path = os.path.join(basedir, "temp", title_for_dir)
-
-    if not os.path.isdir(path):
-        os.mkdir(path)
-
-    img_path = os.path.join(path, "images")
-    if not os.path.isdir(img_path):
-        os.mkdir(img_path)
-    if os.environ.get("DEV"):
-        az_path_base = f"staging/{u.id}/{title_for_dir}/"
-    else:
-        az_path_base = f"{u.id}/{title_for_dir}/"
-    # count for img title
-    j = 0
-    for page in doc:
-        p = page.get_text("dict")["blocks"]
-        for i in p:
-            # checks for images
-            if i["type"] == 1:
-                # if img - get extension and create filename
-                # then increase count in prep for next image.
-                ext = i["ext"]
-                file_name = f"{j}.{ext}"
-                # print(file_name)
-                file_path = f"{img_path}/{file_name}"
-                # print(file_path)
-                j = j + 1
-                # save file to disk.
-                with open(f"{file_path}", "wb") as pic:
-                    # print('opened')
-                    pic.write(i["image"])
-                    # print('saved image')
-                az_path = az_path_base + file_name
-                # print(az_path)
-                s3.upload_file(Bucket=bucket, Filename=file_path, Key=az_path)
-                url = f"/download/{u.id}/{az_path}"
-                content += f'<img src ={url} loading="lazy">'
-            else:
-                lines = i["lines"]
-                if lines is None:
-                    # print(p)
-                    # print(i)
-                    break
-                font_size = None
-                for line in lines:
-                    spans = line["spans"]
-                    for span in spans:
-                        if font_size is None:
-                            font_size = span["size"]
-                            # open tag
-                            content += "<"
-                            content += sizes[font_size]
-                            content += ">"
-                            content += span["text"]
-
-                            # close tag
-                            content += "</"
-                            content += sizes[font_size]
-                            content += ">"
-                        else:
-                            if span["size"] == font_size:
-                                # open tag
-                                content += "<"
-                                content += sizes[font_size]
-                                content += ">"
-
-                                content += " "
-                                content += span["text"]
-
-                                # close tag
-                                content += "</"
-                                content += sizes[font_size]
-                                content += ">"
-                            else:
-                                font_size = span["size"]
-
-                                # open tag
-                                content += "<"
-                                content += sizes[font_size]
-                                content += ">"
-
-                                content += span["text"]
-
-                                # close tag
-                                content += "</"
-                                content += sizes[font_size]
-                                content += ">"
+                        tag = sizes.get(span["size"], "p")
+                        content += f"<{tag}>{span['text']}</{tag}>"
 
     processed["content"] = content
     processed["title"] = title
 
     return processed
+
+
+def process_images_and_get_url_dict(pdf_doc, title_for_dir, user):
+    if not user:
+        user = current_user
+
+    az_path_base = get_path(title_for_dir, user)
+    img_count = 0
+
+    url_dict = {}
+
+    for page in pdf_doc:
+        page_contents = page.get_text("dict")["blocks"]
+        for item in page_contents:
+            if item["type"] == 1:
+                ext = item["ext"]
+                file_name = f"{img_count}.{ext}"
+                img_count += 1
+
+                # Create a BytesIO buffer and write the image data to it
+                buffer = BytesIO()
+                buffer.write(item["image"])
+
+                # Upload the data from the buffer to S3
+                az_path = az_path_base + file_name
+                s3.put_object(Bucket=bucket, Key=az_path, Body=buffer.getvalue())
+
+                url = f"/download/{user.id}/{az_path}"
+                url_dict[item["number"]] = f'<img src ={url} loading="lazy">'
+
+    return url_dict
+
+
+def create_sizes_to_header_tags_dict(pdf_doc):
+    # get all font sizes in doc
+    sizes = {}
+    for page in pdf_doc:
+        page_contents = page.get_text("dict")["blocks"]
+        for item in page_contents:
+            if item["type"] == 0:
+                lines = item["lines"]
+                for line in lines:
+                    spans = line["spans"]
+                    for span in spans:
+                        sizes[span["size"]] = span["size"]
+        for size in sizes:
+            if size > 40:
+                sizes[size] = "h1"
+            elif size < 40 and size > 30:
+                sizes[size] = "h2"
+            elif size < 30 and size > 25:
+                sizes[size] = "h3"
+            elif size < 25 and size > 20:
+                sizes[size] = "h4"
+            elif size < 20 and size > 16:
+                sizes[size] = "h5"
+            else:
+                sizes[size] = "p"
+
+    return sizes
+
+
+def get_titles(pdf_doc):
+    title = pdf_doc.metadata.get("title", "")
+    if title == "":
+        title = pdf_doc.load_page(0).get_text()[:155] + "..."
+
+    title_for_dir = secure_filename(title)
+
+    return title, title_for_dir
+
+
+def get_path(title_for_dir, user):
+
+    if os.environ.get("DEV"):
+        az_path_base = f"staging/{user.id}/{title_for_dir}/"
+    else:
+        az_path_base = f"{user.id}/{title_for_dir}/"
+
+    return az_path_base
---
+++ tests/helpers/test_pdf.py
@@ -0,0 +1,109 @@
+import fitz
+import os
+import shutil
+import tempfile
+import unittest
+from unittest.mock import patch, call, MagicMock
+
+from app import db, create_app
+from app.models import User
+from app.helpers.pdf import (
+    process_images_and_get_url_dict,
+    create_sizes_to_header_tags_dict,
+)
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+    MAIL_SUPPRESS_SEND = True
+
+
+class PDFProcessingTests(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.temp_dir = tempfile.mkdtemp()
+        db.create_all()
+
+        user = User(email="hello@world.com")
+        db.session.add(user)
+        db.session.commit()
+        self.user = user
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+        shutil.rmtree(self.temp_dir)
+
+    def test_process_images(self):
+        pdf_file = os.path.join(os.path.dirname(__file__), "../mocks", "test.pdf")
+        pdf_doc = fitz.open(pdf_file)
+        title_for_dir = "foo_bar_baz"
+
+        expected_staging_dict = {
+            4: '<img src =/download/1/staging/1/foo_bar_baz/0.jpeg loading="lazy">',
+            24: '<img src =/download/1/staging/1/foo_bar_baz/1.jpeg loading="lazy">',
+            25: '<img src =/download/1/staging/1/foo_bar_baz/2.jpeg loading="lazy">',
+            26: '<img src =/download/1/staging/1/foo_bar_baz/3.jpeg loading="lazy">',
+        }
+
+        expected_non_staging_dict = {
+            4: '<img src =/download/1/1/foo_bar_baz/0.jpeg loading="lazy">',
+            24: '<img src =/download/1/1/foo_bar_baz/1.jpeg loading="lazy">',
+            25: '<img src =/download/1/1/foo_bar_baz/2.jpeg loading="lazy">',
+            26: '<img src =/download/1/1/foo_bar_baz/3.jpeg loading="lazy">',
+        }
+
+        with patch("app.helpers.pdf.s3") as mock_s3:
+            url_dict = process_images_and_get_url_dict(
+                pdf_doc, title_for_dir, self.user
+            )
+
+        if os.environ.get("DEV"):
+            self.assertDictEqual(url_dict, expected_staging_dict)
+        else:
+            self.assertDictEqual(url_dict, expected_non_staging_dict)
+
+    def test_create_sizes_to_header_tags_dict(self):
+        # Mock pdf_doc and its get_text method to return test data
+        page1 = {
+            "blocks": [
+                {
+                    "type": 0,
+                    "lines": [
+                        {
+                            "spans": [
+                                {"size": 15},
+                                {"size": 16},
+                                {"size": 17},
+                                {"size": 18},
+                                {"size": 19},
+                                {"size": 24},
+                                {"size": 36},
+                                {"size": 51},
+                            ]
+                        }
+                    ],
+                }
+            ]
+        }
+
+        pdf_doc = [MagicMock(get_text=MagicMock(return_value=page1))]
+
+        # Call the function and check the output
+        expected_sizes = {
+            15: "p",
+            16: "p",
+            17: "h5",
+            18: "h5",
+            19: "h5",
+            24: "h4",
+            36: "h2",
+            51: "h1",
+        }
+        actual_sizes = create_sizes_to_header_tags_dict(pdf_doc)
+        self.assertEqual(expected_sizes, actual_sizes)
